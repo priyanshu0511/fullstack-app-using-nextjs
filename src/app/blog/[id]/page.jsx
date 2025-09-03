dae85e63b9bcf376304232c447e41ed9e52c2aca
priyanshu05112 <priyanshu05112@gmail.com>
2025-09-03 23:30:41 +0530

    added blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,41 @@
+import Image from 'next/image'
 import React from 'react'
 
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
+     <div className="px-20">
+      <div className="flex">
+        <div className="flex-1 flex flex-col justify-between">
+          <h1 className="text-4xl">Title</h1>
+          <p className="text-xl font-light">
+            Description
+          </p>
+          <div className="flex items-center gap-2.5">
+            <Image
+              src="/hero.png"
+              alt=""
+              width={40}
+              height={40}
+              className="object-cover rounded-full"
+            />
+            <span className="">Username</span>
+          </div>
+        </div>
+        <div className="flex-1 h-76 relative">
+          <Image
+            src="/apps.jpg"
+            alt=""
+            fill={true}
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className="mt-12 text-xl font-light text-gray-300 text-center">
+        <p className="">
+         Content
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="px-20">
+        <Link href={`/blog/1`} className="flex items-center gap-12 mb-12 mt-12">
+          <div className="">
+            <Image
+              src="/websites.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className="object-cover"
+            />
+          </div>
+          <div className="">
+            <h1 className="mb-2.5">Title</h1>
+            <p className="text-xl text-gray-400">Description</p>
+          </div>
+        </Link>
+        <Link href={`/blog/2`} className="flex items-center gap-12 mb-12">
+          <div className="">
+            <Image
+              src="/apps.jpg"
+              alt=""
+              width={400}
+              height={250}
+              className="object-cover"
+            />
+          </div>
+          <div className="">
+            <h1 className="mb-2.5">Title</h1>
+            <p className="text-xl text-gray-400">Description</p>
+          </div>
+        </Link>
+        
+    </div>
   )
 }
 
