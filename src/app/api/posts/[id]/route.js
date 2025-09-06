28cfd21961ecbb3e3d07496993bafab43d586c5d
priyanshu05112 <priyanshu05112@gmail.com>
2025-09-07 00:11:29 +0530

    completed functionality

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request,{params}) => {
 
-    const {id}=params;
+    const {id}=await params;
 
   try {
     await connect();
@@ -12,6 +12,21 @@ export const GET = async (request,{params}) => {
     const post = await Post.findById(id);
 
     return new NextResponse(JSON.stringify(post), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
+
+export const DELETE = async (request,{params}) => {
+
+    const {id}=await params;
+
+  try {
+    await connect();
+
+    const post = await Post.findByIdAndDelete(id);
+
+    return new NextResponse("Post has been deleted.", { status: 200 });
   } catch (error) {
     return new NextResponse("Database Error", { status: 500 });
   }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
@@ -39,11 +40,15 @@ const Dashboard = () => {
   const router = useRouter();
 
   console.log(session);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [img, setImg] = useState("");
+  const [content, setContent] = useState("");
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(
-    `/api/posts/username=${session?.data?.user.name}`,
+  const { data, mutate, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
   console.log(data);
@@ -57,8 +62,90 @@ const Dashboard = () => {
   }
 
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete=async(id)=>{
+    try {
+      await fetch(`api/posts/${id}`,{
+        method:"DELETE"
+      })
+      mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   if (session.status === "authenticated") {
-    return <div>Dashboard</div>;
+    return (
+      <div className=" flex gap-24 w-full px-20">
+        <div className="flex-1">
+          {isLoading
+            ? "loading"
+            : data?.map((post) => (
+                <div className="flex items-center justify-between my-12" key={post._id}>
+                  <div className="w-48 h-24 relative">
+                    <Image src={post.img} alt="" width={200} height={100} className="object-cover" />
+                  </div>
+                  <h2 className="">{post.title}</h2>
+                  <span className="cursor-pointer text-red-600" onClick={()=>handleDelete(post._id)}>
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form className="flex-1 flex flex-col gap-5" onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input
+            type="text"
+            placeholder="Title"
+            className="p-2.5 bg-transparent border-2 border-gray-300  text-gray-300 text-xl font-bold"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Desc"
+            className="p-2.5 bg-transparent border-2 border-gray-300  text-gray-300 text-xl font-bold"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Image"
+            className="p-2.5 bg-transparent border-2 border-gray-300  text-gray-300 text-xl font-bold"
+            value={img}
+            onChange={(e) => setImg(e.target.value)}
+          />
+          <textarea
+            placeholder="Content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="p-2.5 bg-transparent border-2 border-gray-300  text-gray-300 text-xl font-bold"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className="p-5 cursor-pointer bg-green-500 border-0 rounded-md text-gray-200 font-bold">Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
